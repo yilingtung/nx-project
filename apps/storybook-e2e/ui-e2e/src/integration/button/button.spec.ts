e2439e03062b40d38d0177f663adf930157c1243
Irene Tung <irene277299@gmail.com>
2022-04-24 19:16:05 +0800

    fix: remove storybook-e2e.

--- apps/storybook-e2e/ui-e2e/src/integration/button/button.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('shared-ui: Button component', () => {
-  beforeEach(() =>
-    cy.visit('/iframe.html?id=button--default&args=children:Hello World')
-  );
-
-  it('should render the component', () => {
-    cy.get('button').should('contain', 'Hello World');
-  });
-});
