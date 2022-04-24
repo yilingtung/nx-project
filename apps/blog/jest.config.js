7bb493526ca30e0c5da80b760ea944ad819be2ac
Irene Tung <irene277299@gmail.com>
2022-04-24 19:08:18 +0800

    chore: add jest mock file.

--- apps/blog/jest.config.js
+++ apps/blog/jest.config.js
@@ -4,7 +4,14 @@ module.exports = {
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+    '.+\\.(png|jpg|gif|ttf|woff|woff2|mp4)$': '<rootDir>/../../jest/stub.js',
+    '^.+\\.svg$': '<rootDir>/../../jest/svgTransformer.js',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/blog',
+  moduleNameMapper: {
+    // '^.+\\.(css|less|sass|scss)$': '<rootDir>/../../../../jest/fileMock.js',
+    '^.+\\.(png|jpg|gif|ttf|woff|woff2|mp4)$':
+      '<rootDir>/../../jest/fileMock.js',
+  },
 };
--- libs/shared/ui/jest.config.js
+++ libs/shared/ui/jest.config.js
@@ -2,8 +2,15 @@ module.exports = {
   displayName: 'shared-ui',
   preset: '../../../jest.preset.js',
   transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
+    '.+\\.(png|jpg|gif|ttf|woff|woff2|mp4)$': '<rootDir>/../../../jest/stub.js',
+    '^.+\\.svg$': '<rootDir>/../../../jest/svgTransformer.js',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/libs/shared/ui',
+  moduleNameMapper: {
+    '^.+\\.(png|jpg|gif|ttf|woff|woff2|mp4)$':
+      '<rootDir>/../../../jest/fileMock.js',
+  },
 };
