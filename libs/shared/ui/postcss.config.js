437b9cb268e6424d072979d5563bb5efab1db8e8
Irene Tung <irene277299@gmail.com>
2022-04-28 09:55:05 +0800

    fix: support tailwind nesting css.

--- libs/shared/ui/postcss.config.js
+++ libs/shared/ui/postcss.config.js
@@ -3,6 +3,7 @@ const { join } = require('path');
 module.exports = {
   plugins: {
     'postcss-import': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {
       config: join(__dirname, 'tailwind.config.js'),
     },
