5a1c0cf3a992b6e550f5183a0dad6bb2cc7d49a0
Irene Tung <irene277299@gmail.com>
2022-04-29 22:12:44 +0800

    fix: add missing tailwind config.

--- tailwind-workspace-preset.js
+++ tailwind-workspace-preset.js
@@ -2,29 +2,29 @@ const plugin = require('tailwindcss/plugin');
 
 module.exports = {
   theme: {
-    colors: {
-      primary: {
-        darker: '#0053ad',
-        dark: '#0169db',
-        DEFAULT: '#007aff',
-        light: '#3797ff',
-        lighter: '#e4f1ff',
-      },
-      netural: {
-        100: '#0d0d0d',
-        90: '#262626',
-        80: '#383838',
-        70: '#6a6a6f',
-        60: '#878787',
-        50: '#c4c4c4',
-        40: '#e2e2e2',
-        30: '#eaeaea',
-        20: '#f6f6f6',
-        10: '#fff',
-      },
-    },
-    borderRadius: { DEFAULT: '4px', full: '9999px' },
     extend: {
+      colors: {
+        primary: {
+          darker: '#0053ad',
+          dark: '#0169db',
+          DEFAULT: '#007aff',
+          light: '#3797ff',
+          lighter: '#e4f1ff',
+        },
+        netural: {
+          100: '#0d0d0d',
+          90: '#262626',
+          80: '#383838',
+          70: '#6a6a6f',
+          60: '#878787',
+          50: '#c4c4c4',
+          40: '#e2e2e2',
+          30: '#eaeaea',
+          20: '#f6f6f6',
+          10: '#fff',
+        },
+      },
+      borderRadius: { DEFAULT: '4px', full: '9999px' },
       animation: {
         'spin-slow': 'spin 3s linear infinite',
       },
